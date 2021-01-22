2277ad4e73c3e8034c761c1ca72ad8cd94356dd9
Johan Åkerman <joha.aker@gmail.com>
2021-01-22 21:02:21 +0100

    changed welcoming text

--- src/components/Welcoming/Welcoming.jsx
+++ src/components/Welcoming/Welcoming.jsx
@@ -9,7 +9,7 @@ export default function Welcoming(props) {
             <div className={styles.content}>
                 <img className={styles.image} src={illustration} />
                 <Typography variant="h3" component="h5" gutterBottom>Welcome to Homerun! </Typography>
-                <Typography variant="body1">Homerun lets you make a "homerun" on your next apartment purchase by helping you compare apartments sold in Stockholm during 2020. Get started by adding a search filter by clicking on the button in the right bottom corner. You can add multiple filters by clicking on the buttom multiple times!</Typography>
+                <Typography variant="body1">Homerun lets you make a "homerun" on your next apartment purchase by helping you compare apartments sold in Stockholm during 2020. Get started by adding a search filter by clicking on the button in the bottom right corner. You can add multiple filters by clicking on the buttom multiple times!</Typography>
             </div>
         </div>
     )
